1168f0e9c28d7be88be3717d62193ee7bb654064
GrachevaElena <alyona-gra98@yandex.ru>
2017-10-03 09:31:22 +0300

    win32

--- CHM9/scr/schet.cpp
+++ CHM9/scr/schet.cpp
@@ -35,10 +35,9 @@ double RK2(Function f, double h, double x, double y)
 	return y + h * f(x + h*0.5, Euler(f, h*0.5, x, y));
 }
 
-Row DoubleCount(Method method, Function f, int p, Row* R)
+Row DoubleCount(Method method, Function f, int p,double h_, Row* R)
 {
 	int x2 = 0, x05 = 0;
-	double h_ = R->hi_1;
 	double y1 = method(f, h_, R->xi, R->viPr);
 	double y2 = method(f, h_*0.5, R->xi + h_*0.5, method(f, h_*0.5, R->xi, R->viPr));
 	double e_ = (y2 - y1) * (pow(2,p)-1);
@@ -75,9 +74,10 @@ void Integrate(Method method, Function f, double x0, double maxX, double y0, int
 	eps = eps_;
 	SetParam(a1,a2,m,u0);
 	int i = 0;
+	double tmph1 = h0, tmph2=h0;
 	Row tmp;
 	tmp.i = 0;
-	tmp.hi_1 = h0;
+	tmp.hi_1 = 0;
 	tmp.xi = x0;
 	tmp.viPr = y0;
 	tmp.viKor = y0;
@@ -94,7 +94,10 @@ void Integrate(Method method, Function f, double x0, double maxX, double y0, int
 	while (tmp.xi<maxX - epsX && i<maxI)
 	{
 		i++;
-		tmp = DoubleCount(method, f, p, &tmp);
+		tmp = DoubleCount(method, f, p, tmph1, &tmp);
+		tmph2 = tmp.hi_1;
+		tmp.hi_1 = tmph1;
+		tmph1 = tmph2;
 		tmp.i = i;
 		tmp.ui = y0*exp(tmp.xi);
 		tmp.abs_ui_vi = abs(tmp.ui - tmp.viPr);
